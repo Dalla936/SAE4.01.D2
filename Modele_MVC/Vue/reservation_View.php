5eb31f6a6c82a60d1e7c52115a22c145bc0e715a
IliasseB <iliasse.bellouch@edu.univ-paris13.fr>
2025-06-15 15:42:02 +0200

    Ajout ou mise à jour de reservation_View et reservation_styles

--- Modele_MVC/Vue/reservation_View.php
+++ Modele_MVC/Vue/reservation_View.php
@@ -7,7 +7,7 @@ $gameName = isset($_GET['game']) ? htmlspecialchars($_GET['game']) : '';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="reservation_styles.css">
+    <link rel="stylesheet" href="reservation-styke.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flatpickr/4.6.13/flatpickr.min.css">
     <title>Réservation - Sorbonne Paris Nord</title>
     <style>
